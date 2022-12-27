ca5f2b7c39bb81cfd47e73cc98ca5f6d4dd9fc77
Yang Yu <reyoung@126.com>
2022-12-27 18:32:52 +0800

    Polish store, make left padding

--- transformersrl/store.py
+++ transformersrl/store.py
@@ -1,7 +1,7 @@
 import torch
 from transformersrl.types import PPOSample, PPOBatch, QuerySample, QueryBatch
 from torch.utils.data.dataloader import DataLoader, Dataset
-from typing import Union, List
+from typing import Union, List, Optional
 from torch.nn.utils.rnn import pad_sequence
 from transformers import PreTrainedTokenizer, DataCollatorWithPadding, PreTrainedTokenizerFast
 
@@ -11,9 +11,23 @@ Sample = Union[PPOSample, QuerySample]
 Batch = Union[PPOBatch, QueryBatch]
 
 
+def left_pad_sequence(sequence, padding_value: int):
+    max_length = max(len(seq.shape[0]) for seq in sequence)
+    stacks: List[Optional[torch.Tensor]] = [None] * len(sequence)
+    for i, seq in enumerate(sequence):
+        pad_length = max_length - seq.shape[0]
+        if pad_length == 0:
+            stacks.append(seq)
+        else:
+            with torch.no_grad():
+                pad = torch.nn.ConstantPad1d(pad_length, padding_value)
+                stacks.append(pad(seq))
+    return torch.stack(stacks)
+
+
 def ppo_collator(samples: List[PPOSample], pad_token: int, **kwargs) -> PPOBatch:
     return PPOBatch(
-        query=pad_sequence([sample.query for sample in samples], batch_first=True, padding_value=pad_token),
+        query=left_pad_sequence([sample.query for sample in samples], padding_value=pad_token),
         response=pad_sequence([sample.response for sample in samples], batch_first=True, padding_value=pad_token),
         probs=pad_sequence([sample.probs for sample in samples], batch_first=True, padding_value=0.0),
         values=pad_sequence([sample.values for sample in samples], batch_first=True, padding_value=0.0),
@@ -25,7 +39,9 @@ def ppo_collator(samples: List[PPOSample], pad_token: int, **kwargs) -> PPOBatch
 def query_collator(samples: List[QuerySample], tokenizer: Union[PreTrainedTokenizer, PreTrainedTokenizerFast],
                    max_length: int,
                    **kwargs) -> QueryBatch:
-    collator = DataCollatorWithPadding(tokenizer, max_length=max_length, **kwargs)
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.padding_side = "left"
+    collator = DataCollatorWithPadding(tokenizer, max_length=max_length, padding="max_length", **kwargs)
     batch = collator([{"text": sample.text} for sample in samples])
     return QueryBatch(input_ids=batch["text"], pad_id=tokenizer.pad_token_id)
 
