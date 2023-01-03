6347703a9df3a01aa02842790890dbdab8f6c073
Yang Yu <reyoung@126.com>
2023-01-03 11:34:14 +0800

    Add transformer rl setup py

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
 from setuptools import setup
 
-
-setup()
+setup(
+    name="transformersrl",
+    version="0.0.1",
+    install_requires=[
+        "transformers",
+        "torch",
+        "numpy",
+        "wandb",
+        "optuna",
+        "datasets",
+        "tqdm",
+    ]
+)
