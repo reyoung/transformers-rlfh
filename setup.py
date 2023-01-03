af2e424a3683dc42e1a73f4daffb6f597bb78f5e
Yang Yu <reyoung@126.com>
2023-01-03 11:50:21 +0800

    Polish version trigger ci

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="transformersrl",
-    version="0.0.1",
+    version="0.0.1.dev.0",
     install_requires=[
         "transformers",
         "torch",
