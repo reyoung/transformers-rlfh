742d2059e26e325b3477854bf7aec8c83ab27d79
Yang Yu <reyoung@126.com>
2023-01-03 12:09:01 +0800

    Refine exclude package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="transformers-rlfh",
     version="0.0.1.dev.0",
-    packages=find_packages(exclude=["tests", "tests.*"]),
+    packages=find_packages(exclude=["tests", "tests.*", "test"]),
     description="RLFH with transformers",
     long_description="RLFH with transformers",
     install_requires=[
